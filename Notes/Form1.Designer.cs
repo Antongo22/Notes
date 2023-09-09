f3d9861b0626c6889c1e8fb0283db8925889b50d
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-09-09 19:44:29 +0300

    есть верхнее меню!

--- Notes/Form1.Designer.cs
+++ Notes/Form1.Designer.cs
@@ -28,9 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заметкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заметкуСДатойToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьПросроченныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.удалитьПросроченныеToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.заметкуToolStripMenuItem,
+            this.заметкуСДатойToolStripMenuItem});
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // заметкуToolStripMenuItem
+            // 
+            this.заметкуToolStripMenuItem.Name = "заметкуToolStripMenuItem";
+            this.заметкуToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.заметкуToolStripMenuItem.Text = "Заметку";
+            // 
+            // заметкуСДатойToolStripMenuItem
+            // 
+            this.заметкуСДатойToolStripMenuItem.Name = "заметкуСДатойToolStripMenuItem";
+            this.заметкуСДатойToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.заметкуСДатойToolStripMenuItem.Text = "Заметку с датой";
+            // 
+            // удалитьПросроченныеToolStripMenuItem
+            // 
+            this.удалитьПросроченныеToolStripMenuItem.Name = "удалитьПросроченныеToolStripMenuItem";
+            this.удалитьПросроченныеToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.удалитьПросроченныеToolStripMenuItem.Text = "Удалить просроченные";
             // 
             // Form1
             // 
@@ -39,18 +91,29 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximumSize = new System.Drawing.Size(900, 700);
             this.MinimumSize = new System.Drawing.Size(900, 700);
             this.Name = "Form1";
             this.Text = "Главная";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заметкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заметкуСДатойToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьПросроченныеToolStripMenuItem;
     }
 }
 
