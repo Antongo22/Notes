9a454f354adc7315922ae39c8362b95203ccbb2c
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-09-10 20:29:01 +0300

    создаёт так, как надо

--- Notes/Form3.cs
+++ Notes/Form3.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,44 +28,52 @@ namespace Notes
         private void buttonSave_Click(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(textBoxName.Text) && !string.IsNullOrEmpty(textBoxText.Text))
-            {               
-                try
+            {
+              
+
+                // SQL-запрос INSERT
+                string insertQuery = $"INSERT INTO Notes (name, path) VALUES (N'{textBoxName.Text}', N''); SELECT SCOPE_IDENTITY();";
+
+                using (SqlCommand insertCommand = new SqlCommand(insertQuery, Form1.sqlConnection))
                 {
-                    // Form1.sqlConnection.Open();
-
-                    // Создание пути с использованием текущего id 
-                    int id = ++Data.id; 
-                    string path = $"note{id}.txt";
-
-                    // SQL-запрос INSERT
-                    string insertQuery = $"INSERT INTO Notes (name, path) VALUES (N'{textBoxName.Text}', N'{path}')";
-
-                    using (SqlCommand command = new SqlCommand(insertQuery, Form1.sqlConnection))
+                    try
                     {
+                        // Выполняем вставку и получаем сгенерированный Id
+                        int insertedId = Convert.ToInt32(insertCommand.ExecuteScalar());
 
-                        int rowsAffected = command.ExecuteNonQuery();
-
-                        if (rowsAffected > 0)
+                        if (insertedId > 0)
                         {
-                            MessageBox.Show("Запись успешно добавлена в таблицу.");
+                            // Обновляем path с учетом Id
+                            string updatedPath = $"note{insertedId}.txt";
+                            string updatePathQuery = $"UPDATE Notes SET path = N'{updatedPath}' WHERE Id = {insertedId}";
+
+                            using (SqlCommand updateCommand = new SqlCommand(updatePathQuery, Form1.sqlConnection))
+                            {
+                                int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Запись успешно добавлена в таблицу.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Произошла ошибка при обновлении пути записи.");
+                                }
+                            }
                         }
                         else
                         {
                             MessageBox.Show("Произошла ошибка при добавлении записи.");
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Произошла ошибка: " + ex.Message);
-                    throw ex;
-                }
-                finally
-                {
-
-                    Form1.sqlConnection.Close();
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Произошла ошибка: " + ex.Message);
+                    }
                 }
             }
+            
+        
             else
             {
                 labelWarning.Visible = true;
